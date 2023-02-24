68501a2b32cd6b21d98f7b8c3b9fb1ffe80ba5d9
Michaela B <michaelabeyer87@gmail.com>
2023-02-24 15:05:32 +0100

    Added Color Constants and assets

---
+++ constants/Colors.js
@@ -0,0 +1,10 @@
+export default {
+  Pink200: "#E9D8E2",
+  Pink400: "#DDB2CC",
+  Violet200: "#B4ADE1",
+  Violet400: "#8D749E",
+  Violet600: "#74668E",
+  Violet800: "#4B3B59",
+  Black: "#252525",
+  White: "#FAFAFA",
+};
